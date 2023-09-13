b86bbbe78f80f20c5c85b02197a456071704f1b2
Yun Peng <pcloudy@google.com>
2023-09-13 10:11:25 +0200

    Fix bazel-skylib with Bazel@HEAD

--- tests/analysis_test_test.sh
+++ tests/analysis_test_test.sh
@@ -58,6 +58,21 @@ EOF
 exports_files(["*.bzl"])
 EOF
 
+  mkdir -p lib
+  cat > lib/BUILD <<EOF
+exports_files(["*.bzl"])
+EOF
+  cat > lib/types.bzl <<EOF
+_a_tuple_type = type(())
+
+def _is_tuple(v):
+    return type(v) == _a_tuple_type
+
+types = struct(
+    is_tuple = _is_tuple,
+)
+EOF
+
   ln -sf "$(rlocation $TEST_WORKSPACE/rules/analysis_test.bzl)" rules/analysis_test.bzl
 
   mkdir -p fakerules
