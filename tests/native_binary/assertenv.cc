20acd194aa94d7bd3e86ea66cb84a25c78fb608a
Paolo Tranquilli <redsun82@github.com>
2023-12-21 15:03:59 +0100

    Generalize again test to windows

--- tests/native_binary/assertenv.cc
+++ tests/native_binary/assertenv.cc
@@ -2,30 +2,30 @@
 #include <string.h>
 
 #ifdef _WIN32
-bool check_home(const char *env) { return true; }
+#define OS_VAR "APPDATA"
+#define strncasecmp _strnicmp
 #else
-bool check_home(const char *env) {
-  return strncmp(env, "HOME=", strlen("HOME=")) == 0;
-}
+#define OS_VAR "HOME"
 #endif
 
 int main(int argc, char **argv, char **envp) {
   bool test_env_found = false;
-  bool home_found = false;
+  bool inherited_var_found = false;
   for (char **env = envp; *env != NULL; ++env) {
+    printf("%s\n", *env);
     if (strcmp(*env, "TEST_ENV_VAR=test_env_var_value") == 0) {
       test_env_found = true;
     }
-    if (check_home(*env)) {
-      home_found = true;
+	if (strncasecmp(*env, OS_VAR "=", strlen(OS_VAR "=")) == 0) {
+      inherited_var_found = true;
     }
   }
   if (!test_env_found) {
     fprintf(stderr,
             "expected TEST_ENV_VAR=test_env_var_value in environment\n");
   }
-  if (!home_found) {
-    fprintf(stderr, "expected HOME in environment\n");
+  if (!inherited_var_found) {
+    fprintf(stderr, "expected " OS_VAR " in environment\n");
   }
-  return test_env_found && home_found ? 0 : 1;
+  return test_env_found && inherited_var_found ? 0 : 1;
 }
